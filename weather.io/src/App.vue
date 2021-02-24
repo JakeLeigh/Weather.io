6e2b7e530d3e2167dd0d082f024077a6f3762675
Jake Leigh <jake@jakeleigh.io>
2021-02-24 14:55:10 +0000

    install axios

--- weather.io/src/App.vue
+++ weather.io/src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" name="" id="" class="search-bar" placeholder='Search...'/>
+        <input 
+          type="text"
+          class="search-bar" 
+          placeholder='Search...' 
+          v-model='query'
+          @keypress='fetchWeather'
+        />
       </div>
       <div class="weather-wrap">
 
@@ -31,6 +37,13 @@ export default {
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e){
+      if(e.key == 'Enter'){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`)
+      }
     }
   }
 }
