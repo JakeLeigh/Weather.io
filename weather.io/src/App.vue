39f2128e67ce7bb921d93b35ac5d77d50af65f26
Jake Leigh <jake@jakeleigh.io>
2021-02-24 15:16:47 +0000

    add logic to conditionally change background image based on weather temp

--- weather.io/src/App.vue
+++ weather.io/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{'warm': typeof weather.main != 'undefined' && weather.main.temp > 16}">
     <main>
       <div class="search-box">
         <input 
@@ -78,6 +78,9 @@ body{
  background-size: cover;
  background-position: bottom;
  transition: .4s;
+}
+#app.warm {
+   background-image: url('./assets/warm-bg.jpg'); 
 }
 main {
   min-height: 100vh;
