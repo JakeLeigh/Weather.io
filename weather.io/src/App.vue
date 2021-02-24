cb34c464a763ab5a5da251dc96699b3362c85c58
Jake Leigh <jake@jakeleigh.io>
2021-02-24 15:08:25 +0000

    save json returned to data object, set conditional statement to show weather data if data object is not empty, output elements of weather

--- weather.io/src/App.vue
+++ weather.io/src/App.vue
@@ -10,16 +10,14 @@
           @keyup.enter='fetchWeather'
         />
       </div>
-      <div class="weather-wrap">
-
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Peterborough, UK</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">Monday 20 January 2020</div>
         </div>
-
         <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
+          <div class="weather">{{weather.weather[0].description}}</div>
         </div>
 
       </div>
@@ -42,14 +40,14 @@ export default {
   },
   methods: {
     fetchWeather (){
-        axios.get(`${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`)
+      axios.get(`${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`)
         .then(res => {
-          console.log(res.data);
+          this.weather = res.data;
         })
         .catch(err => {
           console.log(err);
         })
-      }
+    }
   }
 }
 </script>
