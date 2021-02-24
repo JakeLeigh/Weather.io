2f3aeaac91a85186b7259252fd55a438dd90f7e3
Jake Leigh <jake@jakeleigh.io>
2021-02-24 14:44:09 +0000

    add location and weather boxes to view and add custom styles

--- weather.io/src/App.vue
+++ weather.io/src/App.vue
@@ -3,7 +3,19 @@
     <main>
       <div class="search-box">
         <input type="text" name="" id="" class="search-bar" placeholder='Search...'/>
-        
+      </div>
+      <div class="weather-wrap">
+
+        <div class="location-box">
+          <div class="location">Peterborough, UK</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
+
       </div>
     </main>
   </div>
@@ -64,5 +76,41 @@ main {
   background-color: rgba(255,255,255,.75);
   box-shadow: 0 0 16px rgba(0,0,0,.25);
   border-radius: 16px 0 16px 0;
+}
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0,.25);
+}
+.location-box .date {
+    color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box{
+  text-align: center;
+}
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0,0,0,.25);
+  background-color: rgba(255,255,255,.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0,0,0,.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0,0,0,.25);
 }
 </style>
