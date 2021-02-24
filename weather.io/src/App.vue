15cc8729c0a4b58bf75b77175610091aa44d33c9
Jake Leigh <jake@jakeleigh.io>
2021-02-24 15:13:10 +0000

    wire up func call in date div

--- weather.io/src/App.vue
+++ weather.io/src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="date">{{dateBuilder()}}</div>
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
